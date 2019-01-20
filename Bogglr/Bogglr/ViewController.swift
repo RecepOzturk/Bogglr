0b4f3b4e4634c754ab376ffd00ff7f4f979cb334
Recep <recpozturk@gmail.com>
2019-01-20 15:11:49 +0300

    test code transfered to viewcontroller

--- Bogglr/Bogglr/ViewController.swift
+++ Bogglr/Bogglr/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tree = TrieTree(dictionary: AppEnvironment.shared.dictionary)
+        let board = Board(letters: AppEnvironment.shared.lettersMatrix)
+        let solver = BoggleSolver(board: board, tree: tree)
+        let validWords = solver.findValidWords()
+        
+        print("Here are the valid Boggle words in the board")
+        print("********************************************")
+        validWords.forEach { print($0) }
+        print("number of valid words = \(validWords.count)")
+        print("********************************************")
     }
 
     override func didReceiveMemoryWarning() {
